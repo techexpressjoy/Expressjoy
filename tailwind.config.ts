6c50f9efa64ad51fbb9d6d396eed60de82fa66c7
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2025-02-02 13:17:25 +0530

    feat:add nextjs project structure

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import {heroui} from '@heroui/theme';
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/components/(accordion|badge|button|calendar|card|input|skeleton|divider|ripple|spinner|form).js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [heroui()],
+} satisfies Config;
