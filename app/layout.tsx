22eb6df115d57c704c9904ae592357855b13fc80
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2025-02-02 15:01:57 +0530

    feat:add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
--- db/coupan.ts
+++ db/coupan.ts
@@ -1,37 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 interface ICoupan extends mongoose.Document {
-    code: string;
-    discount: number;
-    active: boolean;
-    expiry: Date;
-    createdAt?: Date;
-    updatedAt?: Date;
+  code: string;
+  discount: number;
+  active: boolean;
+  expiry: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const coupanSchema = new mongoose.Schema(
-    {
-        code : {
-            type : String,
-            required : [true, "Code is required"],
-            unique : true
-        },
-        discount : {
-            type : Number,
-            default : 0
-        },
-       active : {
-            type : Boolean,
-            default : true
-        },
-        expiry : {
-            type : Date,
-            required : [true, "Expiry is required"]
-        },
+  {
+    code: {
+      type: String,
+      required: [true, "Code is required"],
+      unique: true,
     },
-    
-    {timestamps : true})
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    expiry: {
+      type: Date,
+      required: [true, "Expiry is required"],
+    },
+  },
 
-const Coupan = mongoose.model<ICoupan>('Coupan', coupanSchema);
+  { timestamps: true }
+);
+
+const Coupan = mongoose.model<ICoupan>("Coupan", coupanSchema);
 
 export default Coupan;
